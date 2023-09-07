60ec668b1cf8acd276e496a7bdff1e7efa62fc98
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-07 18:00:34 +0500

    Return the number of users with a given age

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -8,4 +8,24 @@ def count_users_with_age(data:list, age:int) -> int:
     Returns:
         int: The number of users with the given age
     """
-    return 
+    age_count = 0
+    for i in data:
+        if i['age']==age:
+            age_count+=1
+    return age_count
+
+print(count_users_with_age(data = [
+  {
+    'name': 'John',
+    'age': 27
+  },
+  {
+    'name':'Mary', 
+    'age': 42
+  },
+  {
+    'name':'Ann',
+    'age': 27
+  }
+  ],
+age = 27))
