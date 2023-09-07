40d9aafdec8c24d3e21fd425cd5449b5ea362fde
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-07 18:26:24 +0500

    Return the sum of the length of all values in a dictionary.

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -6,4 +6,13 @@ def find_length_of_values(data: dict) -> int:
     Returns:
         int: The sum of the length of all values in the dictionary.
     """
-    return 
+    find_length_of_value = 0
+    for i in data:
+        find_length_of_value+=len(data[i])
+    return find_length_of_value
+
+print(find_length_of_values(data = {
+    'a': 'abc',
+    'b': 'def', 
+    'c': 'ghi'
+  }))
