d2f67799edb02ccfd9b5d684bc3e31ba02ed59d3
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-07 19:40:27 +0500

    Return the country of a user with a given name

--- get_user_country.py
+++ get_user_country.py
@@ -8,4 +8,18 @@ def get_user_country(data:list, name:str) -> list:
     Returns:
         str: The country of the user with the given name
     """
-    return 
+    for i in data:
+        if i['name']==name:
+            return i['country'] 
+
+print(get_user_country(data = [
+  {
+    'name': 'John', 
+    'country': 'USA'
+  }, 
+  {
+    'name': 'Mary', 
+    'country': 'UK'
+  }
+],
+name = "John"))
