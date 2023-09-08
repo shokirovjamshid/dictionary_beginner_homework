17f42ac6706d3b399b553bab90ead64a8b82db3b
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-08 12:40:54 +0500

    Return the sum of all values in a dictionary.

--- sum_dict_values.py
+++ sum_dict_values.py
@@ -6,4 +6,16 @@ def sum_dict_values(data: dict) -> int:
     Returns:
         int: The sum of all values in the dictionary
     '''
-    return 
+    sum_values = 0
+    for i in data:
+        sum_values+=data[i]
+    return sum_values
+
+print(sum_dict_values(data = {
+    1: 23, 
+    2: 3.5, 
+    4: 1, 
+    6: 7, 
+    5: 2, 
+    7: 3
+  }))
