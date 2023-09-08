4d6aeffc875bddac287d049e944d325622401d5f
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-08 12:56:04 +0500

    Return the sum of all float values in dictionary.

--- sum_float_values.py
+++ sum_float_values.py
@@ -6,4 +6,17 @@ def sum_float_values(data: dict) -> float:
     Returns:
         float: The sum of all float values in the dictionary.
     '''
-    return 
+    sum_float = 0
+    for i in data:
+        if int(data[i])!=data[i]:
+            sum_float+=data[i]
+    return sum_float
+
+print(sum_float_values(data = {
+    1: 22.4, 
+    2: 3.5, 
+    4: 1, 
+    6: 7.6, 
+    5: 2, 
+    7: 3
+  })) 
