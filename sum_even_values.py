a50a1dc57a4924338bc0456d202c6f5217c8b4fc
shokirovjamshid <shokirovjamshid555@gmail.com>
2023-09-08 12:47:42 +0500

    Return the sum of all even values in a dictionary.

--- sum_even_values.py
+++ sum_even_values.py
@@ -6,4 +6,17 @@ def sum_even_values(data: dict) -> int:
     Returns:
         int: The sum of all even values in the dictionary
     '''
-    return 
+    sum_even = 0
+    for i in data:
+        if data[i]%2==0:
+            sum_even+=data[i]
+    return sum_even
+
+print(sum_even_values(data = {
+    1: 22, 
+    2: 3.5, 
+    4: 1, 
+    6: 7, 
+    5: 2, 
+    7: 3
+  }))
